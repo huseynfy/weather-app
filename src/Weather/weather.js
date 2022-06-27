a70612d65b0678ffa840c749a823b76d074fe1ef
huseynfy <huseyn1996@gmail.com>
2022-06-26 19:15:59 -0700

    adding all the files

--- src/Weather/weather.js
+++ src/Weather/weather.js
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { API_KEY, API_ADDRESS } from "../Data/data";
 import Card from "./card";
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function Weather() {
-  const [lat, setLat] = useState(0);
-  const [lon, setLon] = useState(0);
   const [weatherData,setWeatherData] = useState([])
+  const [currentCity,setCurrentCity] = useState('sacramento');
+
   useEffect(()=>{
     const fetching = async ()=>{
-        navigator.geolocation.getCurrentPosition((position)=>{
-            setLat(position.coords.latitude)
-            setLon(position.coords.longitude)
-        })
-        const resp = await fetch(`${API_ADDRESS}/weather/?lat=${lat}&lon=${lon}&units=imperial&APPID=${API_KEY}`);
+        const resp = await fetch(`${API_ADDRESS}/weather?q=${currentCity.replace(' ','+')}&units=imperial&APPID=${API_KEY}`);
         const data = await resp.json();
         setWeatherData(data)
         console.log(data)
     }
     fetching();
-  },[lat,lon])
+  },[currentCity])
+
+  const handleChange = (e) =>{
+    setCurrentCity(e.target.value);
+  }
 
   return<>
    {(typeof weatherData.main) != 'undefined' ?
-  <Card weatherData={weatherData}/> : <p>No Data</p>}
+  <Card weatherData={weatherData}/> : (
+    <Box sx={{ width: '100%', marginBottom:'32px' }}>
+  <LinearProgress />
+</Box>)}
+  <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '35ch' },
+        marginTop:'32px'
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Search for a city" variant="outlined" onChange={handleChange} />
+    </Box>
   </>
 }
